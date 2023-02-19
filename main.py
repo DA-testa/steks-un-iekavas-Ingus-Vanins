7fccb9b20b9c0613ff139bf90ca51ea0391336cf
Ingus-Vanins <125833785+Ingus-Vanins@users.noreply.github.com>
2023-02-20 00:52:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,26 +12,26 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
+    for s, next in enumerate(text):
         if next in "([{":
             
-            opening_brackets_stack.append(Bracket(next, i + 1))
+            opening_brackets_stack.append(Bracket(next, s + 1))
 
         if next in ")]}":
             
             if not opening_brackets_stack:
                
-                return i + 1
+                return s + 1
+            
             top = opening_brackets_stack.pop()
             if not are_matching(top.char, next):
-              
-                return i + 1
+             
+                return s + 1
 
     if opening_brackets_stack:
         
         return opening_brackets_stack[0].position
 
-    
     return "Success"
 
 def main():
