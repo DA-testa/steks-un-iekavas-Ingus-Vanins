7283834f864af2e81d20d412f920f87c90333ac4
Ingus-Vanins <125833785+Ingus-Vanins@users.noreply.github.com>
2023-02-20 00:50:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,24 +14,24 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket
+            
             opening_brackets_stack.append(Bracket(next, i + 1))
 
         if next in ")]}":
-            # Process closing bracket
+            
             if not opening_brackets_stack:
-                # Case when a closing bracket has no matching opening bracket
+               
                 return i + 1
             top = opening_brackets_stack.pop()
             if not are_matching(top.char, next):
-                # Case when a closing bracket matches the wrong opening bracket
+              
                 return i + 1
 
     if opening_brackets_stack:
-        # Case when there are unmatched opening brackets
+        
         return opening_brackets_stack[0].position
 
-    # Case when all brackets are matched
+    
     return "Success"
 
 def main():
